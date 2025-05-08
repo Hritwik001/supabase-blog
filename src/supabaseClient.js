3755c5e60cc555184328d3f01b5c44bc3ec94185
Hritwik Tiwary <hritwiktiwary001@gmail.com>
2025-05-08 11:03:14 +0530

    Final deploy-ready version using .env

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
-// src/supabaseClient.js
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://qzskgomowfveozmesuuf.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF6c2tnb21vd2Z2ZW96bWVzdXVmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY1OTE4OTcsImV4cCI6MjA2MjE2Nzg5N30.zkThmZX7sCrf3gbAm5OzPWu4SLPEVIhUrq9e2HZg_FU'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseKey)
+
